1bf6e50393e36fbc5e8fcd3fccff27190ded4058
Steven Deseure <deseure.s@gmail.com>
2021-02-08 14:11:38 +0100

    initial commit

---
+++ src/main/java/infernalwaler/springframework/infernalwhalerpetclinic/InfernalwhalerPetClinicApplication.java
@@ -0,0 +1,13 @@
+package infernalwaler.springframework.infernalwhalerpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfernalwhalerPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfernalwhalerPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/infernalwaler/springframework/infernalwhalerpetclinic/InfernalwhalerPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package infernalwaler.springframework.infernalwhalerpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfernalwhalerPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
